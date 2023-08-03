18f1649888b1debc806c75c5f61b4900d902a799
Dale LaPlante <bmbmjmdm@gmail.com>
2023-08-03 18:23:08 +0200

    fix embedding

--- src/Screens/KKGMScreen.tsx
+++ src/Screens/KKGMScreen.tsx
@@ -105,7 +105,7 @@ export const KKGMScreen: FunctionComponent<StackScreenProps<any>> = ({
             <iframe
               width="560"
               height="315"
-              src="https://www.youtube.com/watch?v=OjyGHSUeTyg"
+              src="https://www.youtube.com/embed/OjyGHSUeTyg"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
